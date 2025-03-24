781a6461590a035f9ee4c62f33e10227af382b17
Neamul <neamul.cse6.bu@gmail.com>
2025-03-24 19:08:33 +0600

    Manager and User Dashboard Implemented

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def show_task(request):
-    return HttpResponse("Hello, world! This is a basic HttpResponse.")
+def manager_dashboard(request):
+    return render(request, "manager_dashboard.html")
+def user_dashboard(request):
+    return render(request, "user_dashboard.html")
